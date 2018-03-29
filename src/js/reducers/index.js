d7c247c4ab782c2ee1c12846bee64f28bea981cb
davidmarsland <14043765+davidmarsland@users.noreply.github.com>
2018-03-29 12:16:05 -0700

    removed reducer impl, actions, and constants.  They'll be in next branch

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -6,17 +6,6 @@ const initialState = {
   articles: []
 };
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_ARTICLE:
-      return {
-        ...state,
-        articles: [...state.articles, action.payload]
-      };
-    default:
-      return state;
-  }
-};
-
+const rootReducer = (state = initialState, action) => state;
 
 export default rootReducer;
